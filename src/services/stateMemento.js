a1851cb4aa21d7442a3a80b815e5951530259cf3
IgorBabkin <igba14@gmail.com>
2018-04-24 02:40:41 +0300

    Fix session saving

--- src/services/stateMemento.js
+++ src/services/stateMemento.js
@@ -2,16 +2,14 @@ import {generateGrid} from "../reducers/grid.helpers";
 
 export const StateMemento = {
     setState: data => {
-        // localStorage.setItem('state', JSON.stringify(data));
+        localStorage.setItem('state', JSON.stringify(data));
     },
 
     getState: () => {
-        // const {seek, snapshots} = JSON.parse(localStorage.getItem('state')) || {};
+        const {seek, snapshots} = JSON.parse(localStorage.getItem('state')) || {};
         return {
-            // seek: seek || 0,
-            // snapshots: (snapshots && snapshots.length) || [generateGrid()],
-            seek: 0,
-            snapshots: [generateGrid()],
+            seek: seek || 0,
+            snapshots: (snapshots && snapshots.length) ? snapshots : [generateGrid()],
         };
     }
 };
