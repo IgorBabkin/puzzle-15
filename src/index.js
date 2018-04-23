b62ab731baea887ac2d442fbfeb35615efb14f63
IgorBabkin <igba14@gmail.com>
2018-04-23 20:38:20 +0300

    Add redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './components/app/App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducers} from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
