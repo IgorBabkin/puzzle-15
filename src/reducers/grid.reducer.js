ce8f345b42a590be05be714d8372f6b8988a6250
IgorBabkin <igba14@gmail.com>
2018-04-23 22:33:46 +0300

    Add cosmetic improvements

--- src/reducers/grid.reducer.js
+++ src/reducers/grid.reducer.js
@@ -1,9 +1,10 @@
-export const gridReducer = (canUpdateGrid, updateGrid) => (state = [], {type, payload}) => {
-    switch (type) {
-        case 'UPDATE_GRID':
-            return canUpdateGrid(state, payload) ? updateGrid(state, payload) : state;
+export const gridReducer = (canUpdateGrid, updateGrid) =>
+    (state = [], {type, payload}) => {
+        switch (type) {
+            case 'UPDATE_GRID':
+                return canUpdateGrid(state, payload) ? updateGrid(state, payload) : state;
 
-        default:
-            return state;
-    }
-};
+            default:
+                return state;
+        }
+    };
